4b6180d8e92cf9cdeed5c1f117302879dcb84f39
WakMun <Wakas@raspi>
2020-10-26 21:45:06 +0000

    Blinkled finalized

--- basics/blinkLED.sh
+++ basics/blinkLED.sh
@@ -3,7 +3,7 @@
 # Common path for all GPIO access
 BASE_GPIO_PATH=/sys/class/gpio
 
-# Assign names to GPIO pin numbers for each light
+# Assign a name to GPIO pin number
 RED=23
 
 # Assign names to states
@@ -13,44 +13,38 @@ OFF="0"
 # Utility function to export a pin if not already exported
 exportPin()
 {
-  
-  if [ ! -e $BASE_GPIO_PATH/gpio$1 ]; then
-    echo "$1" > $BASE_GPIO_PATH/export
-  fi
+    if [ ! -e /sys/class/gpio/gpio$1 ]; then
+        echo $1 > /sys/class/gpio/export
+    fi
 }
 
-# Utility function to unexport a pin if not already exported
+# Utility function to export a pin if not already exported
 unexportPin()
 {
-  if [ -e $BASE_GPIO_PATH/gpio$1 ]; then
-    echo "$1" > $BASE_GPIO_PATH/unexport
-  fi
+    if [ ! -e /sys/class/gpio/gpio$1 ]; then
+        echo $1 > /sys/class/gpio/unexport
+    fi
 }
 
 # Utility function to set a pin as an output
 setOutput()
 {
-  echo "out" > $BASE_GPIO_PATH/gpio$1/direction
+    echo "out" > /sys/class/gpio/gpio$1/direction
 }
 
 # Utility function to change state of a light
 setLightState()
 {
-  echo $2 > $BASE_GPIO_PATH/gpio$1/value
+    echo $2 > /sys/class/gpio/gpio$1/value
 }
 
-# Utility function to turn all lights off
-allLightsOff()
-{
-  setLightState $RED $OFF
-}
 
 # Ctrl-C handler for clean shutdown
 shutdown()
 {
-  allLightsOff
-  unexportPin $RED
-  exit 0
+    setLightState $RED $OFF
+    unexportPin $RED
+    exit 0
 }
 
 trap shutdown SIGINT
@@ -61,18 +55,16 @@ exportPin $RED
 # Set pins as outputs
 setOutput $RED
 
-# Turn lights off to begin
-allLightsOff
-
 #Calculate the sleep time based on cmd line args
-SLEEP_SEC=`echo 3k $1 1000 /p | dc`
+SLEEP_SEC=`echo 3k $1 2000 /p | dc`
 
 # Loop forever until user presses Ctrl-C
 while [ 1 ]
 do
-  # Red
-  setLightState $RED $ON
-  sleep $SLEEP_SEC
-  setLightState $RED $OFF
-  sleep $SLEEP_SEC
+    # Red
+    setLightState $RED $ON
+    sleep $SLEEP_SEC
+    setLightState $RED $OFF
+    sleep $SLEEP_SEC
+    
 done
